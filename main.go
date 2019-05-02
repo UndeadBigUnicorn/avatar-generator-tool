4b783a0a828093b469664735ac16fcd637ce840a
Kravchenko Danylo <kravchel16@gmail.com>
2019-05-02 18:03:49 +0300

    added download option

--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"bytes"
-	"image/jpeg"
-	"log"
-	"os"
-
 	"github.com/aofei/air"
 	"github.com/aofei/cameron"
+	"image/jpeg"
+	"log"
 )
 
 var a = air.Default
 
 func main() {
-	a.Address = ":" + os.Getenv("PORT")
+	a.Address = ":5000"// + os.Getenv("PORT")
+	a.GET("/download/:Name", download)
 	a.GET("/:Name", identicon)
 	log.Fatal(a.Serve(), nil)
 }
@@ -42,6 +41,31 @@ func identicon(req *air.Request, res *air.Response) error {
 	}
 
 	res.Header.Set("Content-Type", "image/jpeg")
+
+	return res.Write(bytes.NewReader(buf.Bytes()))
+}
+
+func download(req *air.Request, res *air.Response) error {
+
+	res.Header.Set("Access-Control-Allow-Origin", "*")
+	buf := bytes.Buffer{}
+	err:= jpeg.Encode(
+		&buf,
+		cameron.Identicon(
+			[]byte(req.Param("Name").Value().String()),
+			540,
+			60,
+		),
+		&jpeg.Options{
+			Quality: 100,
+		},
+	)
+
+	if err!=nil{
+		log.Fatal("error", nil)
+	}
+
+	res.Header.Set("Content-Disposition", "attachment; filename=avatar.jpeg")
 
 	return res.Write(bytes.NewReader(buf.Bytes()))
 }
